983d1c1c068663f0aeed0d836ea3bd97a34a474d
Shayan Ghosh <shayan.ghosh@berkeley.edu>
2020-06-09 15:54:17 -0700

    debugging main method and added gitignore

--- src/main.py
+++ src/main.py
@@ -1,13 +1,13 @@
-def setup_board():
-    cells = input("Enter cells: ").strip()
+def setup_board(cells):
+    cells = cells.strip()
     assert len(cells) == 9
     board = []
-    i = 0
-    for j in range(3):
-        board.append([])
-        while i == 0 or i % 3 != 0:
-            board[j].append(cells[i])
+    i = -1
+    for j, c in enumerate(cells):
+        if j % 3 == 0:
+            board.append([])
             i += 1
+        board[i].append(c)
     return board
 
 
@@ -32,80 +32,57 @@ def turn(board):
         return 'O'
 
 
-def move(board):
-    loc = input("Enter the coordinates: ").split()
+def move(board, loc):
     x, y = -1, -1
     try:
         x = int(loc[0]) - 1
         y = int(loc[1]) - 1
     except ValueError:
         print("You should enter numbers!")
-        move(board)
+        move(board, loc)
     except IndexError:
         print("Please enter valid coordinates.")
-        move(board)
+        move(board, loc)
     if x not in range(3) or y not in range(3):
         print("Coordinates should be from 1 to 3!")
-        move(board)
+        move(board, loc)
     elif board[2 - y][x] != "_":
         print("This cell is occupied! Choose another one!")
-        move(board)
+        move(board, loc)
     else:
         board[2 - y][x] = turn(board)
 
 
-def find_neighbors(x, y):
-    neighbors = []
-    for i in range(x - 1, x + 2):
-        for j in range(y - 1, y + 2):
-            if x in range(3) and y in range(3) and (i, j) != (x, y):
-                neighbors.append((i, j))
-    return neighbors
-
-
-def visit(board, x, y, visited, counter):
-    if board[x][y] != turn:
-        return False
-    if counter == 3:
-        return True
-    for neighbor in find_neighbors(x, y):
-        if not visited[neighbor[0]][neighbor[1]]:
-            v = visit(board, neighbor[0], neighbor[1], visited, counter + 1)
-            if v:
-                return True
-    return False
-
-
-def check_win(board):
-    visited = []
+def check_win(board, t):
     for i in range(3):
-        visited.append([False, False, False])
-    for x in range(3):
-        for y in range(3):
-            if visit(board, x, y, visited, 1):
-                return True
-    return False
+        across = all([board[i][j] == t for j in range(3)])
+        down = all([board[j][i] == t for j in range(3)])
+        if across or down:
+            return True
+    diag_n = all([board[i][i] == t for i in range(3)])
+    diag_p = all([board[i][2 - i] == t for i in range(3)])
+    return diag_n or diag_p
 
 
 def game_state(board):
-    if check_win(board):
-        print("X wins")
-    elif check_win(board):
-        print("O wins")
+    if check_win(board, 'X'):
+        return "X wins"
+    elif check_win(board, 'O'):
+        return "O wins"
     else:
         e_counter = len([i for row in board for i in row if i == "_"])
         if e_counter > 0:
-            print("Game not finished")
+            return "Game not finished"
         else:
-            print("Draw")
+            return "Draw"
 
 
 def main():
-    board = setup_board()
+    board = setup_board(input("Enter cells: "))
     print_board(board)
-    move(board)
+    move(board, input("Enter the coordinates: ").split())
     print_board(board)
-    game_state(board)
+    print(game_state(board))
 
 
 if __name__ == "__main__":
--- test/test_main.py
+++ test/test_main.py
@@ -1,9 +1,40 @@
 import unittest
+import copy
+from src import main
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def test_setup(self):
+        board = main.setup_board("XXO___XOX")
+        t_board = [['X', 'X', 'O'],
+                  ['_', '_', '_'],
+                  ['X', 'O', 'X']]
+        self.assertEqual(board, t_board)
+        main.print_board(board)
+
+        board2 = main.setup_board("     XXO___XOX ")
+        self.assertEqual(board2, t_board)
+        main.print_board(board2)
+
+    def test_move(self):
+        board = main.setup_board("_________")
+        board_old = copy.deepcopy(board)
+        main.move(board, [1, 1])
+        board_new = main.setup_board("______X__")
+        self.assertEqual(board, board_new)
+        self.assertNotEqual(board, board_old)
+
+    def test_state(self):
+        x_win = main.setup_board("XO_OXOO_X")
+        x_win2 = main.setup_board("XXXXXXXXX")
+        o_win = main.setup_board("O__OXXOX_")
+        not_finished = main.setup_board("XOXOXO___")
+        draw = main.setup_board("XOOOXXOXO")
+        self.assertEqual(main.game_state(x_win), "X wins")
+        self.assertEqual(main.game_state(x_win2), "X wins")
+        self.assertEqual(main.game_state(o_win), "O wins")
+        self.assertEqual(main.game_state(not_finished), "Game not finished")
+        self.assertEqual(main.game_state(draw), "Draw")
 
 
 if __name__ == '__main__':
